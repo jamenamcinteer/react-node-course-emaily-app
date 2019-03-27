0ac86e4da1313ccdf07382ddb1989337b52118f1
Jamena McInteer <jamena@webdevabq.com>
2019-03-27 08:49:39 -0500

    Creating Subdoc Collections

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,11 +10,14 @@ module.exports = app => {
     // check that user has enough credits on hand to create a survey
     // destructure request body
     const { title, subject, body, recipients } = req.body;
-    // create a new instance of a survey
+    // create a new instance of a survey (in memory, not in db yet)
     const survey = new Survey({
       title,
       subject,
-      body
+      body,
+      recipients: recipients.split(",").map(email => ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now()
     });
   });
 };
