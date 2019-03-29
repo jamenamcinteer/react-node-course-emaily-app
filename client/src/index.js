d79d6b2c35cfe5a0ecf33ca71898468ce8509c0f
Jamena McInteer <jamena@webdevabq.com>
2019-03-29 08:41:48 -0500

    Testing Email Sending

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+// Temporary to test back end route that requires auth
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
